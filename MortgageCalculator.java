db2a2793f9a269d1e0b533650a8064c6ab84f8ae
xifler <bomber8184@icloud.com>
2020-08-30 23:24:28 +0800

    MortageCalculator_Updated 8/30/20
    Fully working without any bugs. Complete with formatting.

--- MortgageCalculator.java
+++ MortgageCalculator.java
@@ -1,3 +1,4 @@
+import java.text.NumberFormat;
 import java.util.Scanner;
 
 public class MortgageCalculator{
@@ -5,21 +6,28 @@ public class MortgageCalculator{
     public static void main(String[] args) {
         //Instantation of Classes
         Scanner reader = new Scanner(System.in);
-        Scanner reader1 = new Scanner(System.in);
-        Scanner reader2 = new Scanner(System.in);
+       
+
+        final byte Months_In_Year = 12;
+        final byte percent = 100;
 
 
         System.out.print("Principal: ");
         double principal = reader.nextDouble(); 
-        System.out.print("Monthly Interest rate: ");
-        double interest = (reader1.nextDouble() / 100)/12; //Monthly interest rate
-        System.out.print("Number of payments ");
-        double numPayments = reader2.nextDouble() * 12;
 
-        double monthPayment = principal*(interest*(Math.pow((1+interest),numPayments) / Math.pow((1+interest), numPayments) - 1));
+        System.out.print("Annual Interest rate: ");
+        float annualinterest = reader.nextFloat();
+        float monthlyInterest = annualinterest / percent / Months_In_Year; 
 
-        System.out.println(monthPayment);
+        System.out.print("Period: ");
+        byte period = reader.nextByte();
+        int numberOfPayments = period *Months_In_Year;
 
+        double mortage = principal*(monthlyInterest*Math.pow(1+monthlyInterest, numberOfPayments) / (Math.pow(1+monthlyInterest, numberOfPayments) -1 ));
+
+        String mortageFormatted = NumberFormat.getCurrencyInstance().format(mortage);
+        System.out.print(mortageFormatted);
+        
         
 
 
