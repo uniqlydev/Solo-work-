e3ccd99e0ba195d8804cdf9bb9713f84e810e7d3
xifler <bomber8184@icloud.com>
2020-09-01 19:54:53 +0800

    MortgageCalculator - Update
    Added loops

--- MortgageCalculator.java
+++ MortgageCalculator.java
@@ -11,23 +11,50 @@ public class MortgageCalculator{
         final byte Months_In_Year = 12;
         final byte percent = 100;
 
+        double principal;
+ 
+        
+         while (true) {
+            System.out.print("Principal ($1k - $1M): ");
+            principal = reader.nextDouble();
+            if (principal >= 1_000 && principal <= 1_000_000 )
+                break;
+        }
 
-        System.out.print("Principal: ");
-        double principal = reader.nextDouble(); 
+        float annualinterest;
+        float monthlyInterest;
 
+        while (true) {
         System.out.print("Annual Interest rate: ");
-        float annualinterest = reader.nextFloat();
-        float monthlyInterest = annualinterest / percent / Months_In_Year; 
+        annualinterest = reader.nextFloat();
+        if (annualinterest > 0 && annualinterest <=30 )
+            break;
+        else 
+            System.out.println("Enter a value greater than 0 and less than or equal to 30");
+        }
+        monthlyInterest = annualinterest / percent / Months_In_Year;
 
+        byte period;
+        int numberOfPayments;
+
+        while (true) {
         System.out.print("Period: ");
-        byte period = reader.nextByte();
-        int numberOfPayments = period *Months_In_Year;
+        period = reader.nextByte();
+            if (period >= 1 && period <=30)
+                break;
+            else 
+                System.out.println("Enter a value between 1 and 30. ");
+        }
+        numberOfPayments = period *Months_In_Year;
+        
+       
 
         double mortage = principal*(monthlyInterest*Math.pow(1+monthlyInterest, numberOfPayments) / (Math.pow(1+monthlyInterest, numberOfPayments) -1 ));
 
         String mortageFormatted = NumberFormat.getCurrencyInstance().format(mortage);
         System.out.print(mortageFormatted);
         
+        reader.close();
         
 
 
