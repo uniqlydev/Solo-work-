9512a705501c21c0b9d98285aad984188ff2d3db
xifler <bomber8184@icloud.com>
2020-09-01 16:06:12 +0800

    FizzBuzz FInal
    Fully working. No bugs.

--- FizzBuzz.java
+++ FizzBuzz.java
@@ -1,11 +1,24 @@
+import java.util.Scanner;
+
 public class FizzBuzz {
     public static void main (String[] args) {
+    // If the number is divisible by 5, the result is fizz 
+    // If the number is divisible by 3, the result is buzz
+    // If the number is divisible by both 5 and 3, the result is fizzbuzz
+    // if the number is not divisible by 5 or 3, the number will print instead. 
+        Scanner number = new Scanner(System.in);
+        System.out.print("Number: ");
+        int num = number.nextInt();
     
-        boolean hasTite = true;
-        boolean hasPepe = false;
-        boolean iseligible = hasTite || hasPepe;
+        String fizz = num%5==0 ? "FIZZ" : Integer.toString(num) ; 
+        String buzz = num%3==0 ? "BUZZ" : Integer.toString(num);
+        boolean fizzBuzz = num%3==0 && num%5==0;
 
-        System.out.print(!iseligible);
-
+        if (fizz != "FIZZ")
+            System.out.print(num);
+        else if (buzz != "BUZZ")
+            System.out.print(num);
+        else if (fizzBuzz)
+            System.out.print("FIZZBUZZ"); 
     }
 }
