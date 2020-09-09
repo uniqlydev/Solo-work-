c4b8da688f69bc3d8666d7b0b3c123838698a665
Brendan Castillo <68150564+DanSiego-0@users.noreply.github.com>
2020-09-09 23:37:44 +0800

    Radius Calcu
    Self explanatory

--- ProceduralDecomposition.java
+++ ProceduralDecomposition.java
@@ -2,44 +2,20 @@ import java.util.Scanner;
 
 public class ProceduralDecomposition {
 
-    public static void message(String name) {
-        System.out.println("Welcome to the game, " + name);
-       
-    }
-    public static void message(int x) {
-        System.out.println("you are " + x);
-       
-    }
-    public static int ageCalcu(int first_Num,int second_Num){
-        return first_Num+second_Num;
+
+    public static double getCircumference(double radius){
+        Scanner console = new Scanner(System.in);
+        double Pi = Math.PI;
+        double circumference = 2*Pi*radius;
+        return circumference;
     }
 
     public static void main (String[] args){
         Scanner console = new Scanner(System.in);
-       
-        // int age = 23;
-        // int hisAge = 18;
-        // System.out.println("WHat is your name? ");
-        // String name = console.next();
-        // message(name);
-        // message(age);
-
-
-        // System.out.println();
-        // System.out.println("WHat is his name? ");
-        // name = console.next();
-        // message(name);
-        // message(age);
-
-        System.out.println();
-        System.out.print("Input age: ");
-        int firstNum = console.nextInt();
-        System.out.print("Input his age: ");
-        int secondNum = console.nextInt();
-        System.out.print("Age gap: " + ageCalcu(firstNum,secondNum)+ "\n");
-    
-
-
+        System.out.println("Input Radius:");
+        System.out.print(getCircumference(console.nextDouble()));
+        
+        
     }
     
 }
