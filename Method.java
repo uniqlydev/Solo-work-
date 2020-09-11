b7126cda85aa563bccd371a49b5dc13d2cc2a59c
Brendan Castillo <68150564+DanSiego-0@users.noreply.github.com>
2020-09-11 13:10:25 +0800

    Method param
    Returns void

---
+++ Method.java
@@ -0,0 +1,18 @@
+import java.util.Scanner;
+public class Method {
+
+    public static void goodboy(int x){
+        System.out.println("Your number " + x + " is a good number");
+    }
+    public static void main (String [] args){
+        // Scanner console = new Scanner(System.in);
+        // for (int y = 0; y <3;y++){
+        //     System.out.println("Enter an integer");
+        //     goodboy(console.nextInt());
+        // }
+        // console.close();
+
+        double x = 2.25;
+        System.out.println(Math.ceil(x));
+    }
+}
