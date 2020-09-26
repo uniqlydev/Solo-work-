4035e0c19ec5f651898a01cf0dd361c982a41a85
Bren <bomber8184@icloud.com>
2020-09-27 05:06:39 +0800

    Quick Update
    A quick update for Goldbach Conjunction

--- GoldBach_Conjecutre.java
+++ GoldBach_Conjecutre.java
@@ -1,22 +1,27 @@
+import java.util.Scanner;
+
 public class GoldBach_Conjecutre {
     
     public static void PrimeChecker(int num) {
+        int divisiblecount = 0;
+        boolean isPrime = true;
        for(int x = 1; x<=num; x++) {
-       boolean isPrime = false; 
-       int divisiblecount = 0;
        if (x%num == 0)
             divisiblecount +=1;
-       if (divisiblecount>2)
-            isPrime = true;
-        
-        
        }
+       if (divisiblecount == 2)
+       System.out.println("prime");
+       else if (divisiblecount>2)
+       System.out.println("Not a prime");
     }
 
 
     
-    public static void main (String[] args)
-    {
+    public static void main (String[] args) {
+    Scanner console = new Scanner(System.in);
+    PrimeChecker(console.nextInt());
+    System.out.println("Hello");
+    
 
     }
 }
