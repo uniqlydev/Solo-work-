6411d2c6ff431be5d1e0a6f4e01301ed5a634234
Bren <bomber8184@icloud.com>
2020-09-27 04:42:43 +0800

    Goldbach_Conjecture
    Added goldbach conjecture file

---
+++ GoldBach_Conjecutre.java
@@ -0,0 +1,22 @@
+public class GoldBach_Conjecutre {
+    
+    public static void PrimeChecker(int num) {
+       for(int x = 1; x<=num; x++) {
+       boolean isPrime = false; 
+       int divisiblecount = 0;
+       if (x%num == 0)
+            divisiblecount +=1;
+       if (divisiblecount>2)
+            isPrime = true;
+        
+        
+       }
+    }
+
+
+    
+    public static void main (String[] args)
+    {
+
+    }
+}
