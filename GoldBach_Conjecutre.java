366e6aba0bf71cb59451e9b201bc93cbb16c3c70
Brendan Castillo <68150564+DanSiego-0@users.noreply.github.com>
2020-09-27 19:50:24 +0800

    Major bugs

--- GoldBach_Conjecutre.java
+++ GoldBach_Conjecutre.java
@@ -1,25 +1,40 @@
 import java.util.Scanner;
 
-// A goldbach conjecture is claim that is 
+
 
 public class GoldBach_Conjecutre {
     
     public static void PrimeChecker(int num) {
         int divisiblecount = 0;
-        boolean isPrime = true;
        for(int x = 1; x<=num; x++) {
-       if (x%num == 0)
+       if (num%x == 0)
             divisiblecount +=1;
        }
        if (divisiblecount == 2)
-       System.out.println("prime");
+       System.out.println("Prime");
        else if (divisiblecount!=2)
        System.out.println("Not a prime");
     }
 
+    
+    public static void PrimePairsFinder(int num) {
+         for (int x = 1; x<=num;x++) {
+            for (int y = 2; y<=x; y++) {
+                if (x%y == 0)
+                    System.out.println(y);
+            }
+            
+        }
+    }
     public static void main (String[] args) {
     Scanner console = new Scanner(System.in);
-    PrimeChecker(console.nextInt());
+    System.out.println("Input a number: ");
+    int choice = console.nextInt();
+    PrimeChecker(choice);
+    PrimePairsFinder(choice);
+
+    
+    
     
 
     }
