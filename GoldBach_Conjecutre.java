375d5d6ab3742603da691c7cc0d4c2453e335eeb
Brendan Castillo <68150564+DanSiego-0@users.noreply.github.com>
2020-09-28 16:53:49 +0800

    Update GoldBach
    More methods

--- GoldBach_Conjecutre.java
+++ GoldBach_Conjecutre.java
@@ -1,26 +1,20 @@
-import java.util.Random;
 import java.util.Scanner;
 import java.util.Arrays;
+import java.util.Random;
 
 public class GoldBach_Conjecutre {
 
-<<<<<<< HEAD
-    
-    public static void PrimeNumberList(int num) {   // Give prime numbers list to be used in Prime list
-       int counter = 0;
-       
-       
+    public static int Random(int min) {
+        Random rand = new Random();
+        int firstNum = rand.nextInt(min);
+        int secondNum = rand.nextInt(min);
+        return firstNum;
     }
 
-    public static void main (String[] args) {
-    Scanner console = new Scanner(System.in);
-    System.out.println("Input a number: ");
-    int choice = console.nextInt();
-    PrimeChecker(choice);
-    PrimeNumberList(choice);
-=======
     public static int[] GetTwoPrimes(int num) {
-        int [] arrayPrime =
+        int primeCount = 0;
+        int currNum;
+        int [] numbers = new int[num];
         for (int x = 1; x <= num; x++) {
             boolean isEven = false;
             for (int y = 2; y <= x/2; ++y) {
@@ -30,24 +24,29 @@ public class GoldBach_Conjecutre {
                 }
                 }
             if (isEven != true) {
-                System.out.println(x);
+                currNum = x;
+                numbers[primeCount] = currNum;
+                primeCount++;
+            }
+        }
+        return numbers;
+    }
 
+    public static void ShowPrime(int num) {
+        int numbers [] = GetTwoPrimes(num);
+        for (int i = 0; i<=num; i++) {
+            System.out.println(numbers[i]);
+            if (numbers[i] == 0) {
+                break;
             }
         }
     }
->>>>>>> 448353b235fbd900f7d8514173008033c41c3418
-
-    public static void GetTwoPairs() {
-        Random rand = new Random();
-
-        int rand1 = rand.nextInt();
-
-    }
-
-
     public static void main (String[] args) {
         Scanner console = new Scanner(System.in);
         System.out.print("Enter a number: ");
-        GetTwoPrimes(console.nextInt());
+        int user_Choice = console.nextInt();
+        int numbers [] = GetTwoPrimes(user_Choice);
+        ShowPrime(user_Choice);
+        console.close();
     }
 }
