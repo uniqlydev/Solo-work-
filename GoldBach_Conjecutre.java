b9d5a7293e76cb2afef1028a70ddea392cb47dfb
Dansiego-0 <bomber8184@icloud.com>
2020-10-01 13:21:00 +0800

    Fresh goldbach

--- GoldBach_Conjecutre.java
+++ GoldBach_Conjecutre.java
@@ -24,18 +24,6 @@ public class GoldBach_Conjecutre {
         }
         return numbers;
     }
-    public static int CountPrime(int num) {
-        int [] primeList = GetTwoPrimes(num);
-        int prime = 0;
-        while (true) {
-            int x = 0; 
-            if (primeList[x] == 0) 
-                break;
-            else 
-                x++;
-        }
-        return prime;
-    }
 
     public static void ShowPrime(int num) {
         int numbers [] = GetTwoPrimes(num);
@@ -51,33 +39,36 @@ public class GoldBach_Conjecutre {
         Random rand = new Random();
         int firstDigit = randomArray[rand.nextInt(num)];
         int secondDigit = randomArray[rand.nextInt(num)];
-        // int recentNum = 0;
-        // int recentNum2 = 0;
-        int primeCount = 6;
-        int primeCounter = 0;
+        int recentNum = 0;
+        int recentNum2 = 0;
+        
 
+
+
+    while(true) {
+        
+        for (int x = 0 ; x<=randomArray.length;x++) {
+            if (recentNum == randomArray[x] && recentNum2 == randomArray[x])    
+                break;
         while (true) { // WIll randomize numbers in order to get a combination within the numbers array
-            if (firstDigit != 0 && secondDigit != 0 && firstDigit != secondDigit && firstDigit + secondDigit == num) { // To avoid 0s and having the same digit. 
-                // recentNum = firstDigit;
-                // recentNum2 = secondDigit;
-                System.out.println("Goldbach: " + firstDigit + "+" + secondDigit);  
-                primeCounter++;
-                break;
+
+            if (firstDigit != 0 && secondDigit != 0 && firstDigit != secondDigit && firstDigit + secondDigit == num) {  // To avoid 0s and having the same digit.
+                recentNum = firstDigit;
+                recentNum2 = secondDigit; 
+                System.out.println("Goldbach: " + firstDigit + "+" + secondDigit); 
             }
-            if (primeCounter == primeCount) 
-                break;
+                
             firstDigit = randomArray[rand.nextInt(num)];// if condition not yet true, randomize again. 
             secondDigit = randomArray[rand.nextInt(num)];
-
-
-            // if (recentNum != firstDigit && recentNum2 != secondDigit && recentNum != 0 && recentNum2 != 0 && recentNum+recentNum2 == num) {
-            // System.out.println("Goldbach: " + firstDigit + "+" + secondDigit);
-            // primeCounter++;
-            // }
-
-         
-            
+            if (recentNum == firstDigit && recentNum2 == secondDigit) {
+                firstDigit = randomArray[rand.nextInt(num)];
+                secondDigit = randomArray [rand.nextInt(num)];
+            }  
         }
+      
+        }
+    }
+        
            
     }
 
---
+++ Goldbach.java
@@ -0,0 +1,47 @@
+import java.util.Scanner;
+
+public class Goldbach {
+
+    static boolean PrimeChecker(int num) {
+        for(int x=2; x<=num/2; x++) {
+            if(num%x == 0) {
+                return false;
+            }
+        }
+        
+        return true;
+    }
+
+    static void goldbach(int num) {
+        if(num%2 != 0) { // Not expressible as  sum of two primes
+            System.out.print(
+                " is Not even!");
+            return;
+        }
+        if(num < 2) {
+            System.out.print(
+                " is less than 2!");
+            return;
+        }
+        
+        System.out.println(
+            "\ncan be represented as");
+        
+        for(int x=2; x<n/2 +1; x++) {
+            if(isPrime(num) && isPrime(n-i)) {
+                System.out.println(
+                  "the sum of two primes = "+
+                      i +" + "+ (n-i) );
+            }
+        }
+    }
+
+
+
+
+    public static void main (String [] args) {
+        Scanner console = new Scanner(System.in);
+
+        PrimeChecker(console.nextInt());
+    }
+}
