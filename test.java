b9c014c83c86c6de80eb8c7ea59e6e180d42c8b5
Dansiego-0 <bomber8184@icloud.com>
2020-10-01 17:06:51 +0800

    Update test.java

--- test.java
+++ test.java
@@ -25,6 +25,7 @@ public class test {
     
     public static void main(String[] args) {
         Scanner in = new Scanner(System.in);
+        System.out.println("Please input an even number");
         goldbach(in.nextInt());
     }
 }
