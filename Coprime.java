e2a1c5c100c089e88ed5f7516f5b87a647cc1168
Dansiego-0 <bomber8184@icloud.com>
2020-10-14 12:14:42 +0800

    With scanner

--- Coprime.java
+++ Coprime.java
@@ -1,6 +1,6 @@
+import java.util.Scanner;
+
 public class Coprime {
-
-
     private static boolean DivisibleCheck (int a,int b) {
         boolean Divisibility = false;
         if (b%a == 0) Divisibility = true;
@@ -12,7 +12,6 @@ public class Coprime {
        int xNum = 0;
        int yNum = 0;
        int factors  = 0;
-       
        for (int i = 1;i<=x+y;i++) {
            if (DivisibleCheck(i, x)) xNum = i;;
            if (DivisibleCheck(i, y)) yNum = i;
@@ -22,8 +21,14 @@ public class Coprime {
            System.out.println(check);
        }
 
+    public static void ShowCoprime() {
+        System.out.println("Input two numbers: "); 
+        Scanner console = new Scanner(System.in);
+        Coprime(console.nextInt(), console.nextInt());
+    }
+
     public static void main (String [] args) {
-     Coprime(21,22);
+        ShowCoprime();
     }
     
 }
