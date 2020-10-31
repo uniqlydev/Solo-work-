d1b6b301848ba4bda43984e97225e5bf44bb1113
Dansiego-0 <bomber8184@icloud.com>
2020-10-31 11:18:34 +0800

    Problemset

--- arrays.java
+++ arrays.java
@@ -1,25 +1,62 @@
 public class arrays {
 
 
-    public static int aba (int[] number){
-
-        int sum = 0;
-
-        for (int i = 0;i<=number.length-1;i++) sum+=number[i];
-
-
-        return sum;
-    }
-
-    static void vararg (int [] number) {
+    static void CountLength(int [] number) {
         System.out.println("Number of args: " + number.length);
-        System.out.println(aba(number));
+        String check = number.length > 50 ? "Array X is greater than 50": "Array X is less than 50";
+        System.out.println(check);
     }
+
+    public static boolean isODD (int num) {
+        int factors = 0;
+        for (int x = 1; x<=num; x++) {
+            if (num % x == 0) factors++; 
+            
+        }
+        boolean check = factors > 2 ? true:false;
+
+        return check; 
+    }
+
+    public static int OddCounter(int [] number) {
+        int oddcount = 0;
+     for (int i = 0; i <= number.length-1;i++) {
+         isODD(number[i]);
+         if (isODD(number[i])) oddcount++;
+     }
+     System.out.println("ODD: " + oddcount);
+        return oddcount; 
+    }
+
+    public static int PrimeCounter(int [] number) {
+        int primecount = 0; 
+        for (int i = 0 ; i<= number.length-1;i++) {
+            isODD(number[i]); 
+            if (!isODD(number[i])) primecount++; 
+        }
+        System.out.println("Prime: " + primecount);
+        return primecount;
+    }
+  
+    public static void Display(int [] num) {
+        for (int x = 0; x<=num.length-1;x++)  if (!isODD(num[x])) System.out.print("+num[x]);
+        for (int y = 0; y<=num.length-1;y++) if (isODD(num[y])) System.out.print();
+           
+        
+    }
+
+       
+    
 
     public static void main (String [] args) {
-        int [] numberarr = {1,2,3,4,5};
+        int[] x = {23,56,67,89,44,11,29,78,34,90,45,88};
+        CountLength(x);
+        PrimeCounter(x);
+        OddCounter(x);
     
-        vararg(numberarr);
+        
+
+       
 
     }
     
