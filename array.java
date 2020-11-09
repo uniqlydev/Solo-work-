f69c82e3a23d8df66773731caab733aea8bd3302
Brendan Castillo <68150564+DanSiego-0@users.noreply.github.com>
2020-11-09 19:28:08 +0800

    pset

--- array.java
+++ array.java
@@ -1,19 +1,76 @@
-import java.util.Scanner;
 
 public class array {
-    
-    public static void main (String [] args) {
-        Scanner scanner = new Scanner(System.in);
 
-        double [] temperature = new double[3];
+    public static int CountNumGreaterThan50 (int [] array) {
+        int numGreaterThan50 = 0;
 
-        for(int x = 0;x<=temperature.length-1;x++) {
-            System.out.println("Enter " + x + " temperature: ");
-            temperature[x] = scanner.nextDouble();
+        for (double T : array)   if (T>50) numGreaterThan50++;
+ 
+        return numGreaterThan50;
+    }
+
+    public static int CountNumLessThan50 (int [] array) {
+        int numGreaterThan50 = CountNumGreaterThan50(array);
+        int numLessThanOrEqualTo50 = array.length-numGreaterThan50;
+
+        return numLessThanOrEqualTo50;   
+    }
+
+    public static boolean isOdd(int x) {
+        boolean isDivisible = x%2!=0 ? true:false;
+        
+        return isDivisible;
+    }
+
+    public static boolean isPrime (int x ) {
+        int factors = 0;
+        for (int i = 1; i<=x;i++) {
+            if (i%x==0) factors++;
+            
         }
+        boolean hasOnly2Factors = factors>2 ? true:false;
 
-        for (int x = 0;x<=temperature.length-1;x++) System.out.println(temperature[x]);
+        return hasOnly2Factors;
+    }
+    public static int PrimeCount (int [] x) {
+        int primeCount = 0;
+        for (int prime:x) if (isPrime(prime)) primeCount++;
+        return primeCount;
+    }
 
-        System.out.println();
+    public static boolean isDivisibleByFive (int x) {
+        boolean isDivisble = x%5==0 || x%10==0 ? true:false;
+
+        return isDivisble;
+    }
+
+    public static void main (String [] args) {
+        int[] x = {23,56,67,89,44,11,29,78,34,90,45,88};
+        int oddCount = 0;
+        int evenCount=0;
+        int primeCount = PrimeCount(x);
+        
+        System.out.println("Number of x Greater than 50: " + CountNumGreaterThan50(x));
+        System.out.println("Number of x less than or equal to 50: " + CountNumLessThan50(x));
+        for(int temp : x) {
+            if (isOdd(temp)) oddCount++;
+            if (!isOdd(temp)) evenCount++;
+        }
+        System.out.println("Odd numbers: " + oddCount+"\nEven numbers: " + evenCount);
+
+        System.out.println("Prime count: " + primeCount);
+
+        System.out.println("Prime numbers: ");
+        for(int prime : x) if (isPrime(prime)) System.out.print(prime+",");
+
+        System.out.println("Numbers divisble by 5: ");
+        for (int i : x) if(isDivisibleByFive(i)) System.out.print(i+",");
+
+            
+
+
+
+        
+        
     }
 }
